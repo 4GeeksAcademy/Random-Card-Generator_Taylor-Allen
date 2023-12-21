62a45e2eff49a78e5fd09e528900b64bcde558b8
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2023-12-21 01:54:46 +0000

    still need to fix button

--- src/app.js
+++ src/app.js
@@ -1,54 +1,53 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+document.body.style.backgroundColor = "green";
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
 window.onload = () => {
-  let domainNames;
+  const cardElement = document.querySelector(".card");
 
+  if (cardElement) {
+    const randomSuit = generateRandomSuit();
+    const randomNumber = generateRandomNumber();
+
+    // Replace spaces with underscores in the class name
+    const sanitizedClass = randomSuit.replace(/\s/g, "_");
+
+    cardElement.classList.add(sanitizedClass);
+    cardElement.innerHTML = randomNumber;
+  } else {
+    console.error("Element with class 'card' not found.");
+  }
   document.querySelector("#btn").addEventListener("click", () => {
-    domainNames = generateDomain();
-    displayDomains(domainNames);
+    return generateRandomNumber;
+    return generateRandomSuit;
   });
-};
-
-let generateDomain = () => {
-  let pronoun = ["the", "our", "your", "its", "my"];
-  let adj = ["great", "big", "happy", "ugly", "slow"];
-  let noun = ["book", "dog", "family", "tree", "time"];
-  let dom = [".com", ".net", ".gov", ".io", ".us"];
-
-  let generatedDomains = [];
-
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < dom.length; l++) {
-          let domainName = `${pronoun[i]}${adj[j]}${noun[k]}${dom[l]}`;
-          generatedDomains.push(domainName);
-        }
-      }
-    }
-  }
-
-  generatedDomains = shuffleArray(generatedDomains).slice(0, MAX_DOMAINS);
-
-  return generatedDomains;
-};
-
-let shuffleArray = array => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
-
-let displayDomains = domains => {
-  let domainList = domains.map(domain => `<li>${domain}</li>`).join("");
-  document.querySelector(
-    "#the-domain"
-  ).innerHTML = `<ul style="list-style-type: none; padding: 0;">${domainList}</ul>`;
 };
